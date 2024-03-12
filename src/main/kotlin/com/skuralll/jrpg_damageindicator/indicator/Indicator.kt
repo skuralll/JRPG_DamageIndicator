b741f1dea78d3644822a4b31e70324c22f52aea1
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-03-12 16:25:21 +0900

    Implement despawn

--- src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/Indicator.kt
+++ src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/Indicator.kt
@@ -1,10 +1,27 @@
 package com.skuralll.jrpg_damageindicator.indicator
 
+import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.ProtocolManager
+import com.comphenix.protocol.events.PacketContainer
+import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
+import java.lang.reflect.InvocationTargetException
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+
 
 class Indicator(private val protocolManager: ProtocolManager, private val player: Player, private val vector: Vector) : Vector(vector.x, vector.y, vector.z) {
+
+    // tick count after spawning
+    private var tick = AtomicInteger(0)
+
+    // Entity ID
+    companion object {
+        val ENTITY_COUNTER  = AtomicInteger(-Random().nextInt())
+    }
+    val entityId = ENTITY_COUNTER.getAndDecrement()
 
     // whether to continue updating
     private var _alive = true
@@ -12,7 +29,20 @@ class Indicator(private val protocolManager: ProtocolManager, private val player
         get() = _alive
 
     fun show(){
-
+        val packet = PacketContainer(PacketType.Play.Server.SPAWN_ENTITY)
+        packet.integers.write(0, entityId)
+        packet.uuiDs.write(0, UUID.randomUUID())
+        packet.entityTypeModifier.write(0, EntityType.FIREBALL)
+        packet.doubles
+            .write(0, x)
+            .write(1, y)
+            .write(2, z);
+        try {
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet)
+            player.sendMessage("show: $entityId")
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+        }
     }
 
     fun update() {
@@ -20,10 +50,22 @@ class Indicator(private val protocolManager: ProtocolManager, private val player
             _alive = false
             return;
         }
+        val now = tick.incrementAndGet()
+        if (now > 20) {
+            _alive = false
+            return
+        }
     }
 
     fun hide() {
-
+        val packet = PacketContainer(PacketType.Play.Server.ENTITY_DESTROY)
+        packet.intLists.write(0, listOf(entityId))
+        try {
+            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet)
+            player.sendMessage("hide: $entityId")
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+        }
     }
 
 }
--- src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/IndicatorController.kt
+++ src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/IndicatorController.kt
@@ -9,34 +9,43 @@ import com.skuralll.jrpg_damageindicator.JRPGDamageIndicator
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Entity
+import org.bukkit.util.Vector
 
 class IndicatorController(private val plugin: JRPGDamageIndicator, private val protocolManager: ProtocolManager) {
 
-    private val indicators = mutableSetOf<Indicator>()
+    private var indicators = mutableMapOf<Int, Indicator>() // use hash map. because to avoid ConcurrentModificationException
 
     init{
         // update indicators
         Bukkit.getScheduler().runTaskTimer(plugin, Runnable {
-            indicators.forEach {
-                it.update()
-                if(!it.alive){
-                    despawn(it)
+            val despawns = mutableListOf<Int>()
+            indicators.forEach  { (_, indicator) ->
+                indicator.update()
+                if(!indicator.alive){
+                    despawns.add(indicator.entityId)
                 }
             }
+            despawns.forEach { despawn(it) }
         }, 0, 1)
     }
 
     // spawn indicator
     fun spawn(damager: Player, target: Entity){
-        val indicator = Indicator(protocolManager, damager, target.location.toVector())
-        indicators.add(indicator)
+        val indicator = Indicator(protocolManager, damager, target.location.toVector().add(Vector(0.0, 2.0, 0.0)))
+        indicators[indicator.entityId] = indicator
         indicator.show()
     }
 
     // despawn indicator
+    private fun despawn(entityId: Int){
+        if (indicators.containsKey(entityId)){
+            indicators[entityId]?.hide()
+            indicators.remove(entityId)
+        }
+    }
+
     private fun despawn(indicator: Indicator){
-        indicator.hide()
-        indicators.remove(indicator)
+        despawn(indicator.entityId)
     }
 
 }
