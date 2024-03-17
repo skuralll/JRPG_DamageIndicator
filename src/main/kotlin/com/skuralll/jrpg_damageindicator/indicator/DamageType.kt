ee8a3d1a97ce0817347c94557ca32aa95a8d15a3
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-03-17 16:48:31 +0900

    Change text format of indicator

--- src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/DamageType.kt
+++ src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/DamageType.kt
@@ -10,12 +10,12 @@ enum class DamageType(
     private val iconColor: TextColor,
     private val damageColor: TextColor
 ) {
-    DIRECT_NORMAL("-", TextColor.color(255, 255, 255), TextColor.color(255, 255, 255)),
-    DIRECT_CRITICAL("-", TextColor.color(255, 170, 0), TextColor.color(255, 170, 0));
+    DIRECT_NORMAL("", TextColor.color(255, 255, 255), TextColor.color(255, 255, 255)),
+    DIRECT_CRITICAL("", TextColor.color(255, 170, 0), TextColor.color(255, 170, 0));
 
     // generate text component for indicator
     fun toTextComponent(damage: Double): Component {
         return Component.text(icon).color(iconColor)
-            .append(Component.text("${floor(damage * 10.0) / 10.0}").color(damageColor))
+            .append(Component.text("⋖ ${floor(damage * 10.0) / 10.0} ⋗").color(damageColor))
     }
 }
