6a588b4bb8cab9878fcb270d348cc08873eafeff
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-03-18 10:01:40 +0900

    Add handling projectile damage (arrow, trident)

--- src/main/kotlin/com/skuralll/jrpg_damageindicator/EventListener.kt
+++ src/main/kotlin/com/skuralll/jrpg_damageindicator/EventListener.kt
@@ -2,16 +2,21 @@ package com.skuralll.jrpg_damageindicator
 
 import com.skuralll.jrpg_damageindicator.indicator.DamageType
 import com.skuralll.jrpg_damageindicator.indicator.IndicatorController
+import org.bukkit.entity.AbstractArrow
 import org.bukkit.entity.ArmorStand
+import org.bukkit.entity.Firework
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Player
+import org.bukkit.entity.Projectile
+import org.bukkit.entity.Trident
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 
 class EventListener(private val indicatorController: IndicatorController) : Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
         // check if the entity is a living entity
         val entity = event.entity
@@ -20,8 +25,24 @@ class EventListener(private val indicatorController: IndicatorController) : List
         var damager: Player? = null
         var type: DamageType = DamageType.DIRECT_NORMAL
 
-        // by player direct attack
-        if (event.damager is Player) {
+
+        if (event.damager is Projectile) {
+            // by projectile
+            val projectile = event.damager as Projectile
+            if (projectile.shooter !is Player) return
+            damager = projectile.shooter as Player
+            if (projectile is AbstractArrow) {
+                if (projectile is Trident) {
+                    type = DamageType.TRIDENT_NORMAL
+                } else {
+                    type = DamageType.ARROW_NORMAL
+                    if (event.isCritical) {
+                        type = DamageType.ARROW_CRITICAL
+                    }
+                }
+            }
+        } else if (event.damager is Player) {
+            // by player direct attack
             damager = event.damager as Player
             if (event.isCritical) {
                 type = DamageType.DIRECT_CRITICAL
--- src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/DamageType.kt
+++ src/main/kotlin/com/skuralll/jrpg_damageindicator/indicator/DamageType.kt
@@ -11,7 +11,15 @@ enum class DamageType(
     private val damageColor: TextColor
 ) {
     DIRECT_NORMAL("", TextColor.color(255, 255, 255), TextColor.color(255, 255, 255)),
-    DIRECT_CRITICAL("", TextColor.color(255, 170, 0), TextColor.color(255, 170, 0));
+    DIRECT_CRITICAL("", TextColor.color(255, 170, 0), TextColor.color(255, 170, 0)),
+    ARROW_NORMAL("\uD83C\uDFF9", TextColor.color(255, 255, 255), TextColor.color(255, 255, 255)),
+    ARROW_CRITICAL("\uD83C\uDFF9", TextColor.color(255, 170, 0), TextColor.color(255, 170, 0)),
+    TRIDENT_NORMAL("\uD83E\uDDF9", TextColor.color(255, 255, 255), TextColor.color(255, 255, 255)),
+    FIREWORKS_NORMAL(
+        "\uD83C\uDF86",
+        TextColor.color(255, 255, 255),
+        TextColor.color(255, 255, 255)
+    );
 
     // generate text component for indicator
     fun toTextComponent(damage: Double): Component {
